47317b8b6aa85796905c73ffd3b3644f247e770c
Seth Schwartz <sschwartz214@gmail.com>
2018-07-31 10:53:24 -0600

    changes test to have_button

--- spec/features/genres/admin_sees_form_to_create_genre_spec.rb
+++ spec/features/genres/admin_sees_form_to_create_genre_spec.rb
@@ -10,7 +10,7 @@ require 'rails_helper'
 
 describe "Admin visits genres index page" do
   context 'as admin' do
-    xit 'allows admin to see see form to create genre' do
+    it 'allows admin to see see form to create genre' do
       admin = User.create(username: "Dee", password: 'password', role: 1)
       genre_1 = Genre.create!(name: "rock")
       genre_2 = Genre.create!(name: "jazz")
@@ -19,7 +19,7 @@ describe "Admin visits genres index page" do
 
       visit genres_path
 
-      expect(page).to have_content('Create new genre')
+      expect(page).to have_button('Create new genre')
     end
   end
 end

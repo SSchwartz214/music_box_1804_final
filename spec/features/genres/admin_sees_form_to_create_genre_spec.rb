1e3ea9782fb7dbc9e60ac9c374620806c93e79d6
Seth Schwartz <sschwartz214@gmail.com>
2018-07-31 09:47:43 -0600

    WIP

---
+++ spec/features/genres/admin_sees_form_to_create_genre_spec.rb
@@ -0,0 +1,25 @@
+# As an admin user,
+#   When I visit the same genre index page that visitors see,
+#   I see the same information visitors see,
+#     And I also see a form to create a new genre.
+#
+# Testing requirements:
+# - users should see at least 2 genres listed on the page
+
+require 'rails_helper'
+
+describe "Admin visits genres index page" do
+  context 'as admin' do
+    it 'allows admin to see see form to create genre' do
+      admin = User.create(username: "Dee", password: 'password', role: 1)
+      genre_1 = Genre.create!(name: "rock")
+      genre_2 = Genre.create!(name: "jazz")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit genres_path
+
+      expect(page).to have_content('Create new genre')
+    end
+  end
+end

1bc4374e329264f54462d6787c3e280638ffd11d
Seth Schwartz <sschwartz214@gmail.com>
2018-07-31 10:43:32 -0600

    user story 8 done

--- spec/features/genres/user_visits_genre_show_page_spec.rb
+++ spec/features/genres/user_visits_genre_show_page_spec.rb
@@ -17,4 +17,27 @@ describe 'as a visitor' do
       expect(page).to_not have_content(song_3.title)
      end
   end
+#   As a Visitor,
+#   When I visit a genre show page,
+#     I see the average rating for all songs in this genre
+#
+# Testing requirements:
+# - I should add at least 2 songs for this genre
+# - Also include 1 or more songs NOT associated with this genre to ensure these
+#   other songs are not included in the average rating
+
+  it 'shows the average rating for all songs' do
+    genre_1 = Genre.create!(name: "rock")
+    genre_2 = Genre.create!(name: "jazz")
+    artist = Artist.create(name: 'Journey')
+    song_1 = genre_1.songs.create!(title: "Don't Stop Believing", length: 320, play_count: 390808, rating: 3, artist: artist)
+    song_2 = genre_1.songs.create!(title: "Anyway You Want It", length: 420, play_count: 67908, rating: 4, artist: artist)
+    song_3 = genre_2.songs.create!(title: "Cold Blooded", length: 453, play_count: 6343, artist: artist)
+
+    visit genre_path(genre_1)
+
+    expect(page).to have_content("rating: #{song_1.rating}")
+    expect(page).to have_content("rating: #{song_2.rating}")
+    expect(page).to_not have_content("rating: #{song_3.rating}")
+  end
 end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -5,7 +5,14 @@ describe Genre, type: :model do
     it{should validate_presence_of(:name)}
   end
 
-  # describe 'relationships' do
-  #   it{should belong_to(:artist)}
-  # end
+  describe 'instance methods' do
+    it '#average_song_rating' do
+      genre = Genre.create!(name: "rock")
+      artist = Artist.create(name: 'Journey')
+      song_1 = genre.songs.create!(title: "Don't Stop Believing", length: 320, play_count: 390808, rating: 3, artist: artist)
+      song_2 = genre.songs.create!(title: "Anyway You Want It", length: 420, play_count: 67908, rating: 5, artist: artist)
+
+      expect(genre.average_song_rating).to eq(4)
+    end
+  end
 end
